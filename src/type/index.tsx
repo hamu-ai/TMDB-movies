ecd5da42e00879e7c1b35fc760169180f871b899
mahi kisimoto <km0227km@gmail.com>
2022-07-17 21:20:20 +0900

    ✨ typescript型

---
+++ src/type/index.tsx
@@ -0,0 +1,19 @@
+export type Movies = {
+  title: string;
+  backdrop_path: string;
+  media_type?: string;
+  release_date?: string;
+  first_air_date: string;
+  genre_ids: number[];
+  id: number;
+  name: string;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+};
