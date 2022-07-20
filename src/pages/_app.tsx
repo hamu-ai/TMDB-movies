f5f1211e0500be2eaf55c5dcd891890846d60586
mahi kisimoto <km0227km@gmail.com>
2022-07-20 21:49:27 +0900

    ✨ RecoilRoot設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
 import Heder from "src/components/Heder";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RecoilRoot>
       <Heder />
       <Component {...pageProps} />
-    </>
+    </RecoilRoot>
   );
 }
 
