53f92b24377eb47ffff829ac7894eade2876a6da
mahi kisimoto <km0227km@gmail.com>
2022-07-20 21:50:11 +0900

    ⚡️ recoil　Stateファイル

---
+++ src/atom/MovieState.tsx
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+import { Movies } from "src/type";
+
+export const MoviesState = atom({
+  key: "MoviesState",
+  default: false,
+});
+
+export const MoviesDataState = atom<Movies | null>({
+  key: "MoviesDataState",
+  default: null,
+});
