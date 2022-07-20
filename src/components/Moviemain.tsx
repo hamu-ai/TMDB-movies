dbed574882c8acfedac71388ee1e9e53baaa906f
mahi kisimoto <km0227km@gmail.com>
2022-07-20 09:26:10 +0900

    ✨ useRefでスクロール実装

--- src/components/Moviemain.tsx
+++ src/components/Moviemain.tsx
@@ -1,22 +1,42 @@
 import Image from "next/image";
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
 import { Movies } from "src/type";
-
+import ArrowBackIos from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 type Props = {
   title: string;
   movie: Movies[];
 };
 
 const Moviemain: FC<Props> = ({ title, movie }) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClick = (direction: string) => {
+    if (ref.current) {
+      const scrollLeft = ref.current.scrollLeft;
+      const clientWidth = ref.current.clientWidth;
+
+      const scrollTo =
+        direction === "left"
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+
+      ref.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+    }
+  };
+
   return (
-    <div className=" text-2xl">
+    <div className=" text-2xl  ">
       <h1 className="pl-3">{title}</h1>
-      <div className="flex overflow-x-auto mx-2  my-5 h-full">
+      <div
+        ref={ref}
+        className="flex  mx-3  overflow-x-scroll scrollbar-hide my-5 h-full"
+      >
         {movie.map((movies) => {
           return (
             <div
               key={movies.id}
-              className="relative md:h-40 md:w-screen md:px-20 h-20 w-20  px-20  "
+              className="relative md:h-40 mx-1 md:w-screen md:px-20 h-20 w-20  px-20  "
             >
               <Image
                 src={`https://image.tmdb.org/t/p/w500${
@@ -29,6 +49,26 @@ const Moviemain: FC<Props> = ({ title, movie }) => {
             </div>
           );
         })}
+
+        <ArrowBackIos
+          onClick={() => handleClick("left")}
+          className=" absolute left-5 mt-8  md:mt-16 md:ml-2 transition hover:-translate-y-1 hover:scale-110 cursor-pointer "
+          sx={{
+            "@media screen and (min-width:765px)": {
+              fontSize: 40,
+            },
+          }}
+        />
+
+        <ArrowForwardIosIcon
+          onClick={() => handleClick("right")}
+          className="absolute right-5 mt-8 md:mt-16 md:mr-2   transition hover:-translate-y-1 hover:scale-110 cursor-pointer"
+          sx={{
+            "@media screen and (min-width:765px)": {
+              fontSize: 40,
+            },
+          }}
+        />
       </div>
     </div>
   );
