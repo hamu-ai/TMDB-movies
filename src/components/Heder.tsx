ed1564c874414cc22fc981b56716217928b263a6
mahi kisimoto <km0227km@gmail.com>
2022-07-23 10:35:50 +0900

    ✨ ログアウト設置

--- src/components/Heder.tsx
+++ src/components/Heder.tsx
@@ -1,7 +1,11 @@
+import { Button, Stack } from "@mui/material";
 import { FC, useEffect, useState } from "react";
+import { useAuth } from "src/hook/AuthContext";
 
 const Heder: FC = () => {
   const [isScroll, setScroll] = useState(false);
+
+  const { singOut } = useAuth();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -18,7 +22,12 @@ const Heder: FC = () => {
     ${isScroll ? "bg-red-600 text-white" : "text-red-600"} 
     flex w-screen  h-10 p-10 items-center  fixed z-10`}
     >
-      <h1 className="font-bold text-2xl ">Movies</h1>
+      <h1 className="font-bold text-2xl flex-1 ">Movies</h1>
+      <Stack spacing={2} direction="row">
+        <Button onClick={singOut} variant="contained">
+          ログアウト
+        </Button>
+      </Stack>
     </div>
   );
 };
