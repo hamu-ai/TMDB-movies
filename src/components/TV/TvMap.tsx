6ee19728340c06f266ea455a8884318dd4eb5a2c
mahi kisimoto <km0227km@gmail.com>
2022-08-01 13:55:02 +0900

    ⚡️ TV動的ページ作成

---
+++ src/components/TV/TvMap.tsx
@@ -0,0 +1,37 @@
+import { FC } from "react";
+import Image from "next/image";
+import { useRecoilState } from "recoil";
+import { MoviesDataState, MoviesState } from "src/atom/MovieState";
+import { Movies } from "src/type";
+
+type Props = {
+  tv: Movies;
+};
+
+export const TvMap: FC<Props> = ({ tv }) => {
+  const [movies, setMovies] = useRecoilState(MoviesDataState);
+  const [open, setOpen] = useRecoilState(MoviesState);
+  return (
+    <div
+      onClick={() => {
+        setMovies(tv);
+        setOpen(true);
+      }}
+      className="relative px-6 border  border-white bg-gray-500 hover:bg-black  transition hover:-translate-y-1 hover:scale-110 cursor-pointer "
+    >
+      <p className="absolute top-0   text-sm md:text-base lg:text-md">
+        {tv.name}
+      </p>
+      <div className="relative   p-20 mt-7   mb-10">
+        <Image
+          src={`https://image.tmdb.org/t/p/w500${
+            tv.backdrop_path || tv.poster_path
+          }`}
+          className="rounded-sm object-contain   "
+          layout="fill"
+          alt="error Image"
+        />
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/tv/[id]/index.tsx
@@ -0,0 +1,63 @@
+import { Pagination, Stack } from "@mui/material";
+import Image from "next/image";
+
+import { useEffect, useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { MoviesDataState, MoviesState } from "src/atom/MovieState";
+import { Movies } from "src/type";
+
+import { NextPage } from "next";
+
+import { useRouter } from "next/router";
+import TvIds from "src/components/TV";
+import { TvMap } from "src/components/TV/TVmap";
+
+const Tvmovies: NextPage = () => {
+  const TV = useRecoilValue(MoviesState);
+
+  const [movies, setMovies] = useRecoilState(MoviesDataState);
+  const [container, setContainer] = useState<Movies[]>([]);
+  const [page, setpage] = useState(1);
+  const router = useRouter();
+  const [open, setOpen] = useRecoilState(MoviesState);
+
+  // TVのデータを取得し、ページ遷移するごとにTVapiが持っているpageが動的に変化
+  useEffect(() => {
+    const TVapi = async () => {
+      const data = await fetch(
+        `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=ja&sort_by=popularity.desc&include_video=false&page=${page}`
+      );
+      const res = await data.json();
+      console.log(res);
+
+      setContainer(res.results);
+    };
+    TVapi();
+  }, [page]);
+
+  return (
+    <div>
+      <div className="relative top-16  grid  gap-2  sm:grid-cols-2  md:grid-cols-4 lg:grid-cols-5  ">
+        {container.map((tv) => (
+          <TvMap key={tv.id} tv={tv} />
+        ))}
+        <Stack spacing={2}>
+          <Pagination
+            count={20}
+            showFirstButton
+            showLastButton
+            color="primary"
+            className=" w-screen flex justify-center py-4 bg-red-600"
+            onChange={(e, page) => {
+              setpage(page);
+              router.push(`/tv/${page}`);
+            }}
+          />
+        </Stack>
+      </div>
+      {TV && <TvIds />}
+    </div>
+  );
+};
+
+export default Tvmovies;
