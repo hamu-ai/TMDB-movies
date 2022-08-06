adae7513833d8bf668f2895a0f2d767fbda9ead6
mahi kisimoto <km0227km@gmail.com>
2022-08-06 10:04:19 +0900

    ♻️ スタイリング

--- src/components/Search/SearcMap.tsx
+++ src/components/Search/SearcMap.tsx
@@ -12,37 +12,35 @@ export const SearcMap: FC<Props> = ({ shows }) => {
   const [movies, setMovies] = useRecoilState(MoviesDataState);
   const [open, setOpen] = useRecoilState(MoviesState);
   return (
-    <div className="border border-red-700 bg-gray-600 hover:bg-black  transition hover:-translate-y-1 hover:scale-110 cursor-pointer p-3">
-      <div
-        onClick={() => {
-          setOpen(true);
-          setMovies(shows);
-        }}
-        className="relative w-full h-52 xl:h-60   "
-      >
-        {shows.backdrop_path || shows.poster_path ? (
-          <>
-            <Image
-              src={`https://image.tmdb.org/t/p/w500${
-                shows.backdrop_path || shows.poster_path
-              }`}
-              className="rounded-sm    "
-              layout="fill"
-              alt={`${shows.name || shows.title}`}
-            />
-            <p className="absolute top-0 bg-black bg-opacity-50">
-              {shows?.name || shows?.title}
+    <div
+      onClick={() => {
+        setOpen(true);
+        setMovies(shows);
+      }}
+      className="relative  w-40  h-56 lg:w-52 lg:h-64   mx-auto  border-8 border-red-700 py-3 bg-gray-500 hover:bg-black  Transition    "
+    >
+      {shows.backdrop_path || shows.poster_path ? (
+        <>
+          <Image
+            src={`https://image.tmdb.org/t/p/w500${
+              shows.poster_path || shows.backdrop_path
+            }`}
+            className="rounded-sm"
+            layout="fill"
+            alt={`${shows.name || shows.title}`}
+          />
+          <p className="absolute top-0 bg-black bg-opacity-50">
+            {shows?.name || shows?.title}
+          </p>
+          {shows.vote_average ? (
+            <p className="absolute right-0 bottom-0 bg-orange-600 rounded-lg ">
+              評価{shows.vote_average}
             </p>
-            {shows.vote_average ? (
-              <p className="absolute right-0 bottom-0 bg-orange-600 rounded-lg ">
-                評価{shows.vote_average}
-              </p>
-            ) : null}
-          </>
-        ) : (
-          <div>{shows.name || shows.title}</div>
-        )}
-      </div>
+          ) : null}
+        </>
+      ) : (
+        <div>{shows.name || shows.title}</div>
+      )}
     </div>
   );
 };
