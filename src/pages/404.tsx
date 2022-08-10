e33e09f6b7737e29d13076af13484467dc14a9bf
mahi kisimoto <km0227km@gmail.com>
2022-08-10 11:13:33 +0900

    ⚡️ 404ページ作成

---
+++ src/pages/404.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { ReactElement, useEffect } from "react";
+import { useRouter } from "next/router";
+
+// 404.　Pageは　404エラーページ
+const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <div className=" text-center mt-10 ">
+      <h1>サイトがありません。</h1>
+      <h2>3秒後にホームページに自動で戻ります。</h2>
+      <div className="mt-10">
+        ↓クリックしたらホームページに戻ります
+        <p className="text-red-700 mt-5 ">
+          <Link href="/">
+            <a>ホームページ</a>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+NotFound.getLayout = function getLayout(page: ReactElement) {
+  return <>{page}</>;
+};
+
+export default NotFound;
