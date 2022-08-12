1f7a3f13321638058023f00f9307ad166eab20b1
mahi kisimoto <km0227km@gmail.com>
2022-08-12 15:10:52 +0900

    ✨ Mantine使うため初期設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import AuthProvider from "src/hook/AuthContext";
 import Heder from "src/components/Heder";
 import { NextPage } from "next";
 import { ReactElement, ReactNode } from "react";
+import { MantineProvider } from "@mantine/core";
 
 //　ページごとのレイアウト
 export type NextPageWithLayout = NextPage & {
@@ -24,11 +25,20 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     );
   }
   return (
-    <AuthProvider>
-      <RecoilRoot>
-        <Heder />
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </AuthProvider>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme: "dark",
+      }}
+    >
+      <AuthProvider>
+        <RecoilRoot>
+          <Heder />
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </AuthProvider>
+    </MantineProvider>
   );
 }
