631d28991d4274afb306bcbd9769594d985275ca
mahi kisimoto <km0227km@gmail.com>
2022-08-14 16:48:44 +0900

    ♻️ 不要コード削除

--- src/components/Heder.tsx
+++ src/components/Heder.tsx
@@ -1,11 +1,7 @@
 import { FC, useEffect, useState } from "react";
-import { useAuth } from "src/hook/AuthContext";
 import Nav from "./Nav";
 
-import { Menu, Button } from "@mantine/core";
-
 const Heder: FC = () => {
-  const { singOut } = useAuth();
   const [isScroll, setScroll] = useState(false);
 
   useEffect(() => {
@@ -24,18 +20,6 @@ const Heder: FC = () => {
     flex w-screen  h-10 p-10 items-center  fixed z-10  `}
     >
       <Nav />
-      <div className="mt-3">
-        <Menu shadow="md" width={200}>
-          <Menu.Target>
-            <Button>ログアウト</Button>
-          </Menu.Target>
-          <Menu.Dropdown>
-            <Menu.Item onClick={() => singOut()}>
-              ログアウトしますか？
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-      </div>
     </div>
   );
 };
