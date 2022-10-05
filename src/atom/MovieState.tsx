f7810c2fe2a147f815e43dc2e6a9c88bd64bc303
mahi kisimoto <km0227km@gmail.com>
2022-10-05 22:40:38 +0900

    ⚡️ 状態追加

--- src/atom/MovieState.tsx
+++ src/atom/MovieState.tsx
@@ -1,21 +1,25 @@
 import { atom, selector } from "recoil";
 import { Movies } from "src/type";
 
+// TV Movie　の動画をModalで表示
 export const MoviesState = atom({
   key: "MoviesState",
   default: false,
 });
 
+// TV Movie APIのデータ
 export const MoviesDataState = atom<Movies | null>({
   key: "MoviesDataState",
   default: null,
 });
 
+// true falseによって、ボタンのデザイン変化
 export const SearchState = atom({
   key: "SearchState",
   default: true,
 });
 
+// 検索サイトのTV Movieデータを発見できるAPIの状態変化
 export const SearchDeliberation = selector({
   key: "SearchDeliberation",
   get: ({ get }) => {
@@ -34,7 +38,20 @@ export const SearchDeliberation = selector({
   },
 });
 
+// 設定ページのメールアドレス表示をリアルタイムで変更
 export const SettingState = atom({
   key: "SettingState",
+  default: false,
+});
+
+// 設定ページのアップデートするModal表示
+export const SettingUpdate = atom({
+  key: "SettingUpdate",
+  default: false,
+});
+
+// 設定ページのアクセス確認のmodal表示
+export const SettingConfirmation = atom({
+  key: "SettingConfirmation",
   default: false,
 });
